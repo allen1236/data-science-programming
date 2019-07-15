559897a10eaea51a82f1459d7b3f406f5d56b81f
b07901135 <b07901135@g.ntu.edu.tw>
2019-07-15 21:42:17 +0800

    crawler update

---
+++ crawler/test.R
@@ -0,0 +1,4 @@
+setwd( '~/Documents/DSP/Data_Science_Programming/crawler/' )
+output_path <- './output_crawler/progress_crawler_stackexchange.txt'
+v <- scan( output_path, what='character' )
+print( v )
